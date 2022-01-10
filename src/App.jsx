7add57e61d920bf5941f37b8dda070b2a6f3e2a1
Becca <beccasullivan30@gmail.com>
2022-01-10 17:14:59 +0000

    add route to single topic

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<ArticlesList/>}></Route>
         <Route path='/articles' element={<ArticlesList/>}></Route>
+        <Route path='/topics/:topic' element={<ArticlesList/>}></Route>
       </Routes>
       </BrowserRouter>
     </div>
