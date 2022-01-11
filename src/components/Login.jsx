c799ffd2862cf1c16ff92186a189a23ed3262464
Becca <beccasullivan30@gmail.com>
2022-01-11 21:39:39 +0000

    create login form and logic

---
+++ src/components/Login.jsx
@@ -0,0 +1,33 @@
+import { useContext, useState } from "react";
+import { UserContext } from "../contexts/UserContext";
+import { getUsers } from "../utils/api";
+
+const Login = () => {
+  const { username, setUsername } = useContext(UserContext);
+  const [isInvalid, setIsInvalid] = useState(false);
+
+  const checkUser = (event) => {
+    event.preventDefault();
+    setIsInvalid(false);
+    getUsers().then((usersFromApi) => {
+      const usernames = usersFromApi.map((user) => user.username);
+      const inputtedUser = event.target[0].value;
+      usernames.includes(inputtedUser)
+        ? setUsername(inputtedUser)
+        : setIsInvalid(true);
+    });
+  };
+
+  return (
+    <div>
+      <p>Login</p>
+      <form onSubmit={(event) => checkUser(event)}>
+        <input type="text" name="username" />
+        <button type="submit">Login</button>
+      </form>
+      {isInvalid && <p>Invalid username</p>}
+    </div>
+  );
+};
+
+export default Login;
