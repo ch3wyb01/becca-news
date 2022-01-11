04b1bccc6133535f1b507f4cb81b26bd6d8729c7
Becca <beccasullivan30@gmail.com>
2022-01-11 19:58:06 +0000

    add route to single article page

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ArticlesList from "./components/ArticlesList";
+import SingleArticle from "./components/SingleArticle";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<ArticlesList/>}></Route>
-        <Route path='/articles' element={<ArticlesList/>}></Route>
-        <Route path='/topics/:topic' element={<ArticlesList/>}></Route>
-      </Routes>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ArticlesList />}></Route>
+          <Route path="/articles" element={<ArticlesList />}></Route>
+          <Route path="/topics/:topic" element={<ArticlesList />}></Route>
+          <Route
+            path="/articles/:article_id"
+            element={<SingleArticle />}
+          ></Route>
+        </Routes>
       </BrowserRouter>
     </div>
   );
