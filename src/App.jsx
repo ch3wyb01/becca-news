6a46d3d431fe2c19956d67f3e02177bc27cce0c1
Becca <beccasullivan30@gmail.com>
2022-01-11 21:39:04 +0000

    create username state and context provider

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,33 @@ import NavBar from "./components/NavBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ArticlesList from "./components/ArticlesList";
 import SingleArticle from "./components/SingleArticle";
+import { UserContext } from "./contexts/UserContext";
+import { useState } from "react";
+import Login from "./components/Login";
 
 function App() {
+  const [username, setUsername] = useState(undefined);
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<ArticlesList />}></Route>
-          <Route path="/articles" element={<ArticlesList />}></Route>
-          <Route path="/topics/:topic" element={<ArticlesList />}></Route>
-          <Route
-            path="/articles/:article_id"
-            element={<SingleArticle />}
-          ></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <UserContext.Provider value={{ username, setUsername }}>
+      <div className="App">
+        {username ? (
+          <BrowserRouter>
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<ArticlesList />}></Route>
+              <Route path="/articles" element={<ArticlesList />}></Route>
+              <Route path="/topics/:topic" element={<ArticlesList />}></Route>
+              <Route
+                path="/articles/:article_id"
+                element={<SingleArticle />}
+              ></Route>
+            </Routes>
+          </BrowserRouter>
+        ) : 
+        <Login />}
+      </div>
+    </UserContext.Provider>
   );
 }
 
