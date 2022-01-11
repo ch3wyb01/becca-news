aa425ca57c6eab30dd535af68d0c57ff48e09704
Becca <beccasullivan30@gmail.com>
2022-01-11 19:59:10 +0000

    create basic page

---
+++ src/components/SingleArticle.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getArticleById } from "../utils/api";
+
+const SingleArticle = () => {
+  const { article_id } = useParams();
+
+  const [article, setArticle] = useState({});
+
+  useEffect(() => {
+    getArticleById(article_id).then((articleFromApi) => {
+      setArticle(articleFromApi);
+    });
+  });
+
+  return (
+    <main>
+      <div>
+        <h2>{article.title}</h2>
+        <h3>{article.topics}</h3>
+        <h4>{article.author}</h4>
+        <p>{article.body}</p>
+        <button>{article.votes}</button>
+      </div>
+      <div>
+        <h3>{article.comment_count}</h3>
+      </div>
+    </main>
+  );
+};
+
+export default SingleArticle;
