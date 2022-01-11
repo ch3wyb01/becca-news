38d43de405c37cb0b35e06c46ffca677e5a6324a
Becca <beccasullivan30@gmail.com>
2022-01-11 20:20:58 +0000

    create comment list

---
+++ src/components/CommentsList.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { getCommentsByArticleId } from "../utils/api";
+import CommentCard from "./CommentCard";
+
+const CommentsList = ({ article_id }) => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    getCommentsByArticleId(article_id).then((commentsFromApi) => {
+      setComments(commentsFromApi);
+    });
+  });
+
+  return (
+    <section>
+      {comments.map((comment) => {
+        return <CommentCard comment={comment}/>;
+      })}
+    </section>
+  );
+};
+
+export default CommentsList;
