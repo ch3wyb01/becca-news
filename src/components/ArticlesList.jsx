f930129085a324cf8b202d4d3d7aa54933bd6a57
Becca <beccasullivan30@gmail.com>
2022-01-12 14:01:37 +0000

    add loading state

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -5,31 +5,40 @@ import ArticleCard from "./ArticleCard";
 
 const ArticlesList = () => {
   const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const {topic} = useParams();
+  const { topic } = useParams();
 
   useEffect(() => {
+    setIsLoading(true);
     getArticles(topic).then((articlesFromApi) => {
       setArticles(articlesFromApi);
+      setIsLoading(false);
     });
   }, [topic]);
 
   return (
-    <main>
-      <h1>{topic ? `${topic} articles` : "All articles"}</h1>
-      {articles.map((article) => {
-        return (
-          <ArticleCard
-            key={article.article_id}
-            id={article.article_id}
-            title={article.title}
-            body={article.body}
-            topic={article.topic}
-            showTopic={topic}
-          />
-        );
-      })}
-    </main>
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <main>
+          <h1>{topic ? `${topic} articles` : "All articles"}</h1>
+          {articles.map((article) => {
+            return (
+              <ArticleCard
+                key={article.article_id}
+                id={article.article_id}
+                title={article.title}
+                body={article.body}
+                topic={article.topic}
+                showTopic={topic}
+              />
+            );
+          })}
+        </main>
+      )}
+    </div>
   );
 };
 
