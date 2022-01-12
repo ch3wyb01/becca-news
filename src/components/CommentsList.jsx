c0d8d0669bb2dbb1819723602ea5fa3119aa8759
Becca <beccasullivan30@gmail.com>
2022-01-12 13:56:18 +0000

    add loading state

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -4,19 +4,27 @@ import CommentCard from "./CommentCard";
 
 const CommentsList = ({ article_id }) => {
   const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     getCommentsByArticleId(article_id).then((commentsFromApi) => {
       setComments(commentsFromApi);
+      setIsLoading(false);
     });
   });
 
   return (
-    <section>
-      {comments.map((comment) => {
-        return <CommentCard key={comment.comment_id} comment={comment}/>;
-      })}
-    </section>
+    <main>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <section>
+          {comments.map((comment) => {
+            return <CommentCard key={comment.comment_id} comment={comment} />;
+          })}
+        </section>
+      )}
+    </main>
   );
 };
 
