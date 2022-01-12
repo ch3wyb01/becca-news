3b5d4c70e2e9438ec5a9f30c6a2091a5a1bcb11a
Becca <beccasullivan30@gmail.com>
2022-01-12 16:07:20 +0000

    create add comment functionality

---
+++ src/components/AddComment.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react/cjs/react.development";
+import { postComment } from "../utils/api";
+
+const AddComment = ({ article_id, loadComments}) => {
+  const [comment, setComment] = useState("");
+
+  const handleChange = (event) => {
+    setComment(event.target.value);
+  };
+
+  const addComment = (event) => {
+    event.preventDefault();
+    const body = event.target[0].value;
+    postComment(article_id, body).then(() => {
+      setComment("");
+      loadComments();
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={addComment}>
+        <textarea
+          onChange={handleChange}
+          name="body"
+          cols="30"
+          rows="10"
+          value={comment}
+        ></textarea>
+        <button type="submit">Add comment</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddComment;
