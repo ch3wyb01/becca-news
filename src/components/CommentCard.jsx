f8b64957f984b76567891d393ed905a212d430ce
Becca <beccasullivan30@gmail.com>
2022-01-12 16:36:18 +0000

    add delete functionality

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,8 +1,22 @@
-const CommentCard = ({ comment: { body, author, votes } }) => {
+import { deleteComment } from "../utils/api";
+
+const CommentCard = ({
+  comment: { comment_id, body, author, votes },
+  username,
+  loadComments,
+}) => {
+  
+  const handleDelete = () => {
+    deleteComment(comment_id).then(() => {
+      loadComments();
+    });
+  };
+
   return (
     <div>
       <p>{body}</p>
       <p>{author}</p>
+      {username === author ? <button onClick={handleDelete}>Delete comment</button> : null}
     </div>
   );
 };
