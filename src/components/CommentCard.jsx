db053d7fff74fff86e9c89049abf952bca13104c
Becca <beccasullivan30@gmail.com>
2022-01-13 12:41:06 +0000

    change username prop to useContext

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,11 +1,15 @@
+import { useContext } from "react";
 import { deleteComment } from "../utils/api";
+import { UserContext } from "../contexts/UserContext";
+
 
 const CommentCard = ({
   comment: { comment_id, body, author, votes },
-  username,
   loadComments,
 }) => {
-  
+
+  const {username} = useContext(UserContext);
+
   const handleDelete = () => {
     deleteComment(comment_id).then(() => {
       loadComments();
@@ -16,7 +20,9 @@ const CommentCard = ({
     <div>
       <p>{body}</p>
       <p>{author}</p>
-      {username === author ? <button onClick={handleDelete}>Delete comment</button> : null}
+      {username === author ? (
+        <button onClick={handleDelete}>Delete comment</button>
+      ) : null}
     </div>
   );
 };
