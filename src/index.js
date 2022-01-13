01b325224a62b98d0db506ce76d70f74f455be38
Becca <beccasullivan30@gmail.com>
2022-01-13 12:38:36 +0000

    add dayjs

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const dayjs = require('dayjs');
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
