f45b03b663ee883686497494f415418c3e7b6c4a
Becca <beccasullivan30@gmail.com>
2022-01-14 13:28:20 +0000

    order comments

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -2,15 +2,17 @@ import { useEffect, useState } from "react";
 import { getCommentsByArticleId } from "../utils/api";
 import AddComment from "./AddComment";
 import CommentCard from "./CommentCard";
+import { orderComments } from "../utils/utils";
 
-const CommentsList = ({ article_id }) => {
+const CommentsList = ({ article_id, comment_count }) => {
   const [comments, setComments] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const loadComments = () => {
     setIsLoading(true);
     getCommentsByArticleId(article_id).then((commentsFromApi) => {
-      setComments(commentsFromApi);
+      const latestComments = orderComments(commentsFromApi);
+      setComments(latestComments);
       setIsLoading(false);
     });
   };
@@ -23,14 +25,23 @@ const CommentsList = ({ article_id }) => {
     <main>
       {isLoading ? (
         <div class="spinner-border text-secondary" role="status">
-        <span class="visually-hidden">Loading...</span>
-      </div>
+          <span class="visually-hidden">Loading...</span>
+        </div>
       ) : (
         <div>
-          <AddComment article_id={article_id} loadComments={loadComments}/>
+          <AddComment article_id={article_id} loadComments={loadComments} />
           <section>
+            <div>
+              <h6>{comment_count} comments</h6>
+            </div>
             {comments.map((comment) => {
-              return <CommentCard key={comment.comment_id} comment={comment} loadComments={loadComments}/>;
+              return (
+                <CommentCard
+                  key={comment.comment_id}
+                  comment={comment}
+                  loadComments={loadComments}
+                />
+              );
             })}
           </section>
         </div>
