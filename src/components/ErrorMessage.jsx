26049a499776658200e227990028daee8cc2ec54
Becca <beccasullivan30@gmail.com>
2022-01-14 15:16:40 +0000

    add back to home button

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,5 +1,20 @@
+import {useNavigate} from "react-router-dom";
+
 const ErrorMessage = ({ message }) => {
-  return <p>{message}</p>;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <p>{message ? message : "Oops! We couldn't find that page"}</p>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Back to home
+      </button>
+    </div>
+  );
 };
 
 export default ErrorMessage;
