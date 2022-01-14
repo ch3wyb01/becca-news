a6d1e9336e5b4374a6574736c7d3a8dcdff0ac69
Becca <beccasullivan30@gmail.com>
2022-01-14 11:27:35 +0000

    create card styling

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,5 +1,13 @@
 import { Link } from "react-router-dom";
 import { formatDate } from "../utils/utils";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardSubTitle,
+  MDBBadge,
+  MDBIcon
+} from "mdb-react-ui-kit";
 
 const ArticleCard = ({
   id,
@@ -9,18 +17,24 @@ const ArticleCard = ({
   votes,
   author,
   comment_count,
-  showTopic,
+  hideTopic,
 }) => {
   return (
-    <Link to={`/articles/${id}`}>
-      <div>
-        <h2>{title}</h2>
-        {!showTopic && <p>{topic}</p>}
-        <p>{formatDate(created_at)}</p>
-        <p>Votes: {votes}</p>
-        <p>Comments: {comment_count}</p>
-      </div>
-    </Link>
+ <MDBCard className="shadow my-4 mx-auto px-2">
+   <MDBCardBody>
+     <MDBCardTitle tag='h4' className="mb-1">{title}</MDBCardTitle>
+     <MDBCardSubTitle tag='h6'>By {author}</MDBCardSubTitle>
+     <MDBCardSubTitle>{formatDate(created_at)}</MDBCardSubTitle>
+     <div className={`d-flex ${hideTopic ? "justify-content-end" : "justify-content-between"} px-4`}>
+     {!hideTopic && <MDBBadge color='secondary' className="mb-1">{topic}</MDBBadge>}
+     <div className="d-flex">
+     <MDBCardSubTitle className="mx-2">{votes} <MDBIcon far icon="star" /></MDBCardSubTitle>
+     <MDBCardSubTitle>{comment_count} <MDBIcon far icon="comments" /></MDBCardSubTitle>
+     </div>
+     </div>
+     <a href={`/articles/${id}`} className="stretched-link"></a>
+   </MDBCardBody>
+ </MDBCard>
   );
 };
 
