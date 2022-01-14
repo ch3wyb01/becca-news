65e84741dbd8e27e88c0deba5751bbc7692c43df
Becca <beccasullivan30@gmail.com>
2022-01-14 14:39:35 +0000

    add styling

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -3,6 +3,7 @@ import { getCommentsByArticleId } from "../utils/api";
 import AddComment from "./AddComment";
 import CommentCard from "./CommentCard";
 import { orderComments } from "../utils/utils";
+import { MDBCard, MDBCardBody, MDBCardHeader } from "mdb-react-ui-kit";
 
 const CommentsList = ({ article_id, comment_count }) => {
   const [comments, setComments] = useState([]);
@@ -30,20 +31,21 @@ const CommentsList = ({ article_id, comment_count }) => {
       ) : (
         <div>
           <AddComment article_id={article_id} loadComments={loadComments} />
-          <section>
-            <div>
-              <h6>{comment_count} comments</h6>
-            </div>
-            {comments.map((comment) => {
-              return (
-                <CommentCard
-                  key={comment.comment_id}
-                  comment={comment}
-                  loadComments={loadComments}
-                />
-              );
-            })}
-          </section>
+          <MDBCard className="shadow my-4 mx-2 px-2 pt-4">
+              <h4>Recent comments</h4>
+              <h5>{comment_count} comments</h5>
+            <MDBCardBody>
+              {comments.map((comment) => {
+                return (
+                  <CommentCard
+                    key={comment.comment_id}
+                    comment={comment}
+                    loadComments={loadComments}
+                  />
+                );
+              })}
+            </MDBCardBody>
+          </MDBCard>
         </div>
       )}
     </main>
