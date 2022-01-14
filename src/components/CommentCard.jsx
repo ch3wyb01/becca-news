b1b0d3922c834af07081b5226141d35d720e37f3
Becca <beccasullivan30@gmail.com>
2022-01-14 13:29:30 +0000

    add date and votes

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,10 +1,10 @@
 import { useContext } from "react";
 import { deleteComment } from "../utils/api";
 import { UserContext } from "../contexts/UserContext";
-
+import { formatDate } from "../utils/utils";
 
 const CommentCard = ({
-  comment: { comment_id, body, author, votes },
+  comment: { comment_id, body, author, votes, created_at },
   loadComments,
 }) => {
 
@@ -20,6 +20,8 @@ const CommentCard = ({
     <div>
       <p>{body}</p>
       <p>{author}</p>
+      <p>{formatDate(created_at)}</p>
+      <p>{votes}</p>
       {username === author ? (
         <button onClick={handleDelete}>Delete comment</button>
       ) : null}
