851bf6dcd2199ec27ff1719ca636b6e4aeeaabf7
Becca <beccasullivan30@gmail.com>
2022-01-14 13:25:36 +0000

    add icon button

--- src/components/ArticleVotes.jsx
+++ src/components/ArticleVotes.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { patchArticleVotes } from "../utils/api";
+import { MDBIcon, MDBBtn } from "mdb-react-ui-kit";
 
 const ArticleVotes = ({ id, votes }) => {
   const [articleVotes, setArticleVotes] = useState(votes);
@@ -11,7 +12,7 @@ const ArticleVotes = ({ id, votes }) => {
     });
   };
 
-  return <button onClick={handleClick}>{articleVotes}</button>;
+  return <MDBBtn className="px-2 py-1" onClick={handleClick}>{articleVotes} <MDBIcon far icon="star" /></MDBBtn>;
 };
 
 export default ArticleVotes;
