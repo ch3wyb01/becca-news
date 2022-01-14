fbccc6cc0761af8db09f254df115d4b092b78770
Becca <beccasullivan30@gmail.com>
2022-01-14 13:28:30 +0000

    create orderComments

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,15 @@
 import dayjs from "dayjs";
+import _ from "lodash";
 
 export const formatDate = (ISOstring) => {
   return dayjs(ISOstring).format("DD MMM YYYY");
+};
+
+ export const orderComments = (comments) => {
+  const commentsCopy = [...comments];
+  const orderedComments = _.sortBy(
+    commentsCopy,
+    (comment) => comment.created_at
+  ).reverse();
+  return orderedComments;
 };
