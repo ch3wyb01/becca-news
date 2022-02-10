057ce2e0f584618acb7a8627d251ea5b144a674b
Becca <beccasullivan30@gmail.com>
2022-02-10 15:21:07 +0000

    add logging in spinner

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,8 +5,10 @@ import { getUsers } from "../utils/api";
 const Login = () => {
   const { username, setUsername } = useContext(UserContext);
   const [isInvalid, setIsInvalid] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const checkUser = (event) => {
+    setIsLoading(true);
     event.preventDefault();
     setIsInvalid(false);
     getUsers().then((usersFromApi) => {
@@ -14,20 +16,29 @@ const Login = () => {
       const inputtedUser = event.target[0].value;
       if (usernames.includes(inputtedUser)) {
         setUsername(inputtedUser);
-        sessionStorage.setItem('username', inputtedUser);
+        sessionStorage.setItem("username", inputtedUser);
       } else setIsInvalid(true);
     });
   };
 
   return (
-    <div>
-      <p>Login</p>
-      <form onSubmit={(event) => checkUser(event)}>
-        <input type="text" name="username" />
-        <button type="submit">Login</button>
-      </form>
-      {isInvalid && <p>Invalid username</p>}
-    </div>
+    <main>
+      {isLoading ? (
+        <div>
+          <div class="spinner-border text-secondary" role="status"></div>
+          <p>Logging in...</p>
+        </div>
+      ) : (
+        <div>
+          <p>Login</p>
+          <form onSubmit={(event) => checkUser(event)}>
+            <input type="text" name="username" />
+            <button type="submit">Login</button>
+          </form>
+          {isInvalid && <p>Invalid username</p>}
+        </div>
+      )}
+    </main>
   );
 };
 
