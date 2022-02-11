19829cb055d23b43cc24f1e82bf9852cbd4f815a
Becca <beccasullivan30@gmail.com>
2022-02-11 14:26:54 +0000

    add votes button

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -3,6 +3,8 @@ import { deleteComment } from "../utils/api";
 import { UserContext } from "../contexts/UserContext";
 import { formatDate } from "../utils/utils";
 import { MDBBtn } from "mdb-react-ui-kit";
+import ArticleVotes from "./ArticleVotes";
+import CommentVotes from "./CommentVotes";
 
 const CommentCard = ({
   comment: { comment_id, body, author, votes, created_at },
@@ -20,11 +22,11 @@ const CommentCard = ({
   return (
     <div>
       <div className="d-flex flex-column justify-content-start align-items-start">
-      <h6 class='fw-bold mb-1'>{author}</h6>
+      <h6 className='fw-bold mb-1'>{author}</h6>
       <p><small>{formatDate(created_at)}</small></p>
       </div>
       <p className="text-start">{body}</p>
-      {/* <p>{votes}</p> */}
+      <CommentVotes id={comment_id} votes={votes} author={author}/>
       {username === author ? (
         <MDBBtn color="danger" className="px-2" onClick={handleDelete}>Delete comment</MDBBtn>
       ) : null}
