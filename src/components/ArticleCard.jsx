252ae52c82a671849666415ed06c65588a218a24
Becca <beccasullivan30@gmail.com>
2022-02-11 12:51:25 +0000

    prettier formatting

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -5,7 +5,7 @@ import {
   MDBCardTitle,
   MDBCardSubTitle,
   MDBBadge,
-  MDBIcon
+  MDBIcon,
 } from "mdb-react-ui-kit";
 
 const ArticleCard = ({
@@ -19,21 +19,35 @@ const ArticleCard = ({
   hideTopic,
 }) => {
   return (
- <MDBCard className="shadow my-4 mx-auto px-2">
-   <MDBCardBody>
-     <MDBCardTitle tag='h4' className="mb-1">{title}</MDBCardTitle>
-     <MDBCardSubTitle tag='h6'>By {author}</MDBCardSubTitle>
-     <MDBCardSubTitle>{formatDate(created_at)}</MDBCardSubTitle>
-     <div className={`d-flex ${hideTopic ? "justify-content-end" : "justify-content-between"} px-4`}>
-     {!hideTopic && <MDBBadge color='secondary' className="mb-1">{topic}</MDBBadge>}
-     <div className="d-flex">
-     <MDBCardSubTitle className="mx-2">{votes} <MDBIcon far icon="star" /></MDBCardSubTitle>
-     <MDBCardSubTitle>{comment_count} <MDBIcon far icon="comments" /></MDBCardSubTitle>
-     </div>
-     </div>
-     <a href={`/articles/${id}`} className="stretched-link"></a>
-   </MDBCardBody>
- </MDBCard>
+    <MDBCard className="shadow my-4 mx-auto px-2">
+      <MDBCardBody>
+        <MDBCardTitle tag="h4" className="mb-1">
+          {title}
+        </MDBCardTitle>
+        <MDBCardSubTitle tag="h6">By {author}</MDBCardSubTitle>
+        <MDBCardSubTitle>{formatDate(created_at)}</MDBCardSubTitle>
+        <div
+          className={`d-flex ${
+            hideTopic ? "justify-content-end" : "justify-content-between"
+          } px-4`}
+        >
+          {!hideTopic && (
+            <MDBBadge color="secondary" className="mb-1">
+              {topic}
+            </MDBBadge>
+          )}
+          <div className="d-flex">
+            <MDBCardSubTitle className="mx-2">
+              {votes} <MDBIcon far icon="star" />
+            </MDBCardSubTitle>
+            <MDBCardSubTitle>
+              {comment_count} <MDBIcon far icon="comments" />
+            </MDBCardSubTitle>
+          </div>
+        </div>
+        <a href={`/articles/${id}`} className="stretched-link"></a>
+      </MDBCardBody>
+    </MDBCard>
   );
 };
 
