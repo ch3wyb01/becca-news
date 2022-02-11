a54010120307d82c3a81b40cc711c05090964859
Becca <beccasullivan30@gmail.com>
2022-02-11 14:27:10 +0000

    create votes functionality

---
+++ src/components/CommentVotes.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { patchVotes } from "../utils/api";
+import { MDBIcon, MDBBtn } from "mdb-react-ui-kit";
+import { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
+
+const CommentVotes = ({ id, votes, author }) => {
+  const [commentVotes, setCommentVotes] = useState(votes);
+  const [hasVoted, setHasVoted] = useState(false);
+
+  const { username } = useContext(UserContext);
+
+  const handleClick = () => {
+    if (!hasVoted) {
+      setCommentVotes((currVotes) => currVotes + 1);
+      setHasVoted(true);
+      patchVotes("comments", id, 1).catch(() => {
+        setCommentVotes((currVotes) => currVotes - 1);
+        setHasVoted(false);
+      });
+    } else {
+      setCommentVotes((currVotes) => currVotes - 1);
+      setHasVoted(false);
+      patchVotes("comments", id, -1).catch(() => {
+        setCommentVotes((currVotes) => currVotes + 1);
+        setHasVoted(true);
+      });
+    }
+  };
+
+  return (
+    <MDBBtn
+      className={`px-2 py-1 ${username === author ? "disabled" : ""}`}
+      onClick={handleClick}
+    >
+      {commentVotes} <MDBIcon far icon="star" />
+    </MDBBtn>
+  );
+};
+
+export default CommentVotes;
