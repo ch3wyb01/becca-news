022ecace91b86c6a9770073252f002305b45a077
Becca <beccasullivan30@gmail.com>
2022-02-11 12:51:54 +0000

    add styling

--- src/components/PaginationNav.jsx
+++ src/components/PaginationNav.jsx
@@ -1,29 +1,44 @@
-import { useEffect } from "react";
+import {
+  MDBPaginationItem,
+  MDBPaginationLink,
+} from "mdb-react-ui-kit";
 
 const articleLimit = 10;
 
-const PaginationNav = ({page, setPage, totalCount}) => {
-  useEffect(() => {}, [page]);
-
+const PaginationNav = ({ page, setPage, totalCount }) => {
   return (
-    <div>
-      <button
-        disabled={page === 1}
-        onClick={() => {
-          setPage((currPage) => currPage - 1);
-        }}
-      >
-        Previous
-      </button>
-      <button
-        disabled={articleLimit * page >= totalCount}
-        onClick={() => {
-          setPage((currPage) => currPage + 1);
-        }}
-      >
-        Next
-      </button>
-    </div>
+    <nav aria-label="Page navigation">
+      <ul className="pagination mb-0 mx-5 d-flex justify-content-between">
+        <li className={`d-flex ${page === 1 ? "page-item disabled" : ""}`}>
+          <MDBPaginationItem>
+            <MDBPaginationLink
+              onClick={() => {
+                setPage((currPage) => currPage - 1);
+              }}
+              aria-label="Previous"
+            >
+              <span aria-hidden="true">« Previous</span>
+            </MDBPaginationLink>
+          </MDBPaginationItem>
+        </li>
+        <li
+          className={
+            articleLimit * page >= totalCount ? "page-item disabled" : ""
+          }
+        >
+        <MDBPaginationItem>
+          <MDBPaginationLink
+            onClick={() => {
+              setPage((currPage) => currPage + 1);
+            }}
+            aria-label="Next"
+          >
+            <span aria-hidden="true">Next »</span>
+          </MDBPaginationLink>
+        </MDBPaginationItem>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
