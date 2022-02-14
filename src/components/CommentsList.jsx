7c14c1aa10267583b2b9566563f5c324d4a48ab1
Becca <beccasullivan30@gmail.com>
2022-02-14 12:08:32 +0000

    remove loadComments function

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -2,24 +2,20 @@ import { useEffect, useState } from "react";
 import { getCommentsByArticleId } from "../utils/api";
 import AddComment from "./AddComment";
 import CommentCard from "./CommentCard";
-import { orderComments } from "../utils/utils";
 import { MDBCard, MDBCardBody, MDBCardHeader } from "mdb-react-ui-kit";
 
 const CommentsList = ({ article_id, comment_count }) => {
   const [comments, setComments] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  const loadComments = () => {
+  useEffect(() => {
     setIsLoading(true);
     getCommentsByArticleId(article_id).then((commentsFromApi) => {
       const latestComments = orderComments(commentsFromApi);
       setComments(latestComments);
+      setComments(commentsFromApi);
       setIsLoading(false);
     });
-  };
-
-  useEffect(() => {
-    loadComments();
   }, []);
 
   return (
@@ -30,17 +26,18 @@ const CommentsList = ({ article_id, comment_count }) => {
         </div>
       ) : (
         <div>
-          <AddComment article_id={article_id} loadComments={loadComments} />
+          <AddComment article_id={article_id} setComments={setComments} comments={comments}/>
           <MDBCard className="shadow my-4 mx-2 px-2 pt-4">
-              <h4>Recent comments</h4>
-              <h5>{comment_count} comments</h5>
+            <h4>Recent comments</h4>
+            <h5>{comment_count} comments</h5>
             <MDBCardBody>
               {comments.map((comment) => {
                 return (
                   <CommentCard
                     key={comment.comment_id}
                     comment={comment}
-                    loadComments={loadComments}
+                    setComments={setComments}
+                    comments={comments}
                   />
                 );
               })}
