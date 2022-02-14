9f1c4ea5b84d8bc3e796824cbca26ecfb38c11c5
Becca <beccasullivan30@gmail.com>
2022-02-14 12:22:43 +0000

    remove orderComments function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,12 +4,3 @@ import _ from "lodash";
 export const formatDate = (ISOstring) => {
   return dayjs(ISOstring).format("DD MMM YYYY");
 };
-
-export const orderComments = (comments) => {
-  const commentsCopy = [...comments];
-  const orderedComments = _.sortBy(
-    commentsCopy,
-    (comment) => comment.created_at
-  ).reverse();
-  return orderedComments;
-};
