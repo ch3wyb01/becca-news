5e832f50af3b5be845f566a09f7dbd4a1026a2cd
Becca <beccasullivan30@gmail.com>
2022-02-22 15:50:03 +0000

    add tabs for article lists

--- src/components/Profile/ProfilePage.jsx
+++ src/components/Profile/ProfilePage.jsx
@@ -3,11 +3,23 @@ import { useContext } from "react";
 import { UserContext } from "../../contexts/UserContext";
 import { getUser } from "../../utils/api";
 import UserArticlesList from "./UserArticlesList";
+import {
+  MDBTabs,
+  MDBTabsItem,
+  MDBTabsLink,
+  MDBTabsContent,
+  MDBTabsPane,
+} from "mdb-react-ui-kit";
 
 const ProfilePage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [user, setUser] = useState({});
   const { username } = useContext(UserContext);
+  const [activeTab, setActiveTab] = useState("authored");
+
+  const handleTabClick = (value) => {
+    setActiveTab(value);
+  };
 
   useEffect(() => {
     setIsLoading(true);
@@ -26,8 +38,33 @@ const ProfilePage = () => {
       ) : (
         <main>
           <h1 id="profile-greeting">Welcome back, {user.name}</h1>
-          <UserArticlesList username={username} isAuthor={true}/>
-          <UserArticlesList username={username} isAuthor={false}/>
+          <MDBTabs fill className="mb-3">
+            <MDBTabsItem>
+              <MDBTabsLink
+                onClick={() => handleTabClick("authored")}
+                active={activeTab === "authored"}
+              >
+                Authored Articles
+              </MDBTabsLink>
+            </MDBTabsItem>
+            <MDBTabsItem>
+              <MDBTabsLink
+                onClick={() => handleTabClick("voted")}
+                active={activeTab === "voted"}
+              >
+                Voted Articles
+              </MDBTabsLink>
+            </MDBTabsItem>
+          </MDBTabs>
+
+          <MDBTabsContent>
+            <MDBTabsPane show={activeTab === "authored"}>
+              <UserArticlesList username={username} isAuthor={true} />
+            </MDBTabsPane>
+            <MDBTabsPane show={activeTab === "voted"}>
+              <UserArticlesList username={username} isAuthor={false} />
+            </MDBTabsPane>
+          </MDBTabsContent>
         </main>
       )}
     </div>
