096a278532b1c30ea715914505a8d848200e8da5
Becca <beccasullivan30@gmail.com>
2022-02-22 21:23:25 +0000

    create basic modal template

---
+++ src/components/Main/Modal.jsx
@@ -0,0 +1,17 @@
+import { MDBModal, MDBModalDialog, MDBModalContent } from "mdb-react-ui-kit";
+import { useContext } from "react";
+import { ModalContext } from "../../contexts/ModalContext";
+
+const Modal = ({ children }) => {
+  const { modal } = useContext(ModalContext);
+
+  return (
+    <MDBModal show={modal}>
+      <MDBModalDialog centered>
+        <MDBModalContent>{children}</MDBModalContent>
+      </MDBModalDialog>
+    </MDBModal>
+  );
+};
+
+export default Modal;
